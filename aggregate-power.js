ac41d9023ff0bbc14ad5a7d015a2af20d2eecd60
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-09-07 11:54:23 +0800

    add function to compute storage

--- aggregate-power.js
+++ aggregate-power.js
@@ -11,10 +11,10 @@ const data = [
 const dates = [{date:"01:00"}, {date:"02:00"}, {date:"03:00"}, {date:"04:00"}];
 
 const expected = [
-  {date: "01:00", pv:  0, load: 30,  pvControl:  0, base: 30, buffer:   0},
-  {date: "02:00", pv: 20, load: 70,  pvControl: 10, base: 40, buffer:  10},
-  {date: "03:00", pv: 60, load: 50,  pvControl: 20, base: 30, buffer:  40},
-  {date: "04:00", pv:  0, load: 30,  pvControl: 10, base: 20, buffer: -10}
+  {date: "01:00", pv:  0, load: 30,  pvControl:  0, base: 30, buffer:   0, storage:   0},
+  {date: "02:00", pv: 20, load: 70,  pvControl: 10, base: 40, buffer:  10, storage: -20},
+  {date: "03:00", pv: 60, load: 50,  pvControl: 20, base: 30, buffer:  40, storage:   0},
+  {date: "04:00", pv:  0, load: 30,  pvControl: 10, base: 20, buffer: -10, storage:   0}
 ];
 
 const vc = (idx, i) => data[idx].variation[i] * data[idx].capacity;
@@ -79,15 +79,16 @@ const getBatteryBuffer = R.chain(
 const getBatteryStorage = R.chain(
   R.merge,
   R.compose(
-    R.objOf('battery'),
-    R.reduceRight(R.subtract, 0),
-    R.props(['pv', 'pvControl'])
+    R.objOf('storage'),
+    ([p, b, l]) => (p + b) - l,
+    R.props(['pvControl', 'base', 'load'])
   )
 );
 
 const f = (acc, val, i) => {
   return R.append(
     R.merge(R.compose(
+      getBatteryStorage,
       getBase(R.last(acc)),
       getBatteryBuffer,
       getPVcontrol(R.last(acc)),
