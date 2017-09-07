e684b2a14bafafb856094b541c23e4444db55aef
Pawel Lisewski <pawel.lisewski@gmail.com>
2017-09-07 14:06:30 +0800

    add guard in case no battery

--- aggregate-power.js
+++ aggregate-power.js
@@ -8,8 +8,6 @@ const data = [
   {type: 'battery', capacity: 1000 },
 ];
 
-const dates = [{date:"01:00"}, {date:"02:00"}, {date:"03:00"}, {date:"04:00"}];
-
 const expected = [
   {date: "01:00", pv:  0, load: 30,  pvControl:  0, base: 30, buffer:   0, storage:   0},
   {date: "02:00", pv: 20, load: 70,  pvControl: 10, base: 40, buffer:  10, storage: -20},
@@ -17,90 +15,119 @@ const expected = [
   {date: "04:00", pv:  0, load: 30,  pvControl: 10, base: 30, buffer: -10, storage:  10}
 ];
 
-const vc = (idx, i) => data[idx].variation[i] * data[idx].capacity;
+const dataNoBat= [
+  {type: 'load', capacity: 100, variation: [0.3, 0.7, 0.5, 0.3]},
+  {type: 'pv', capacity: 100, ramp: 0.1, variation: [0.0, 0.2, 0.6, 0.0]},
+  {type: 'base', capacity: 100, ramp: 0.1, base: 0.3}
+];
 
-const getLoad = i => R.assoc('load', vc(0,i));
+const expectedNoBat = [
+  {date: "01:00", pv:  0, load: 30,  pvControl:  0, base: 30, buffer:  0, storage: 0},
+  {date: "02:00", pv: 20, load: 70,  pvControl: 20, base: 40, buffer:  0, storage: 0},
+  {date: "03:00", pv: 60, load: 50,  pvControl: 60, base: 30, buffer:  0, storage: 0},
+  {date: "04:00", pv:  0, load: 30,  pvControl:  0, base: 30, buffer:  0, storage: 0}
+];
 
-const getPV = i => R.assoc('pv', vc(1,i));
+function computeOutput (xs) {
+  const data = R.zipObj(R.pluck('type', xs), xs);
+  const DATES = [{date:"01:00"}, {date:"02:00"}, {date:"03:00"}, {date:"04:00"}];
+  const PV_RAMP = data.pv.capacity * data.pv.ramp;
+  const BASE_RAMP = data.base.capacity * data.base.ramp;
+  const BASE = data.base.capacity * data.base.base;
+  const NO_BATT = R.isNil(data.battery);
 
-const checkRamp = (r,x) => R.compose(R.gte(r), R.compose(Math.abs, R.subtract(x)));
+  const vc = (type, i) => data[type].variation[i] * data[type].capacity;
 
-const computePVRamp = last => R.ifElse(
-  checkRamp(10, last.pvControl),
-  R.identity,
-  R.clamp(R.subtract(last.pvControl, 10), R.add(last.pvControl, 10))
-);
+  const getLoad = i => R.assoc('load', vc('load', i));
 
-const computeBaseRamp = last => R.ifElse(
-  checkRamp(10, last.base),
-  R.identity,
-  R.clamp(R.subtract(last.base, 10), R.add(last.base, 10))
-);
+  const getPV = i => R.assoc('pv', vc('pv', i));
 
-const setBaseRamp = last => R.ifElse(
-  R.always(R.isNil(R.path(['base'], last))),
-  R.identity,
-  (x) => computeBaseRamp(last)(x)
-);
+  const checkRamp = (r, x) => R.compose(R.gte(r), R.compose(Math.abs, R.subtract(x)));
 
-const getPVcontrol = (last) => R.chain(
-  R.merge,
-  R.compose(
-    R.objOf('pvControl'),
-    R.ifElse(
-      R.always(R.isNil(R.path(['pvControl'], last))),
-      R.identity,
-      (x) => computePVRamp(last)(x)
-    ),
-    R.prop('pv')
-  )
-);
-
-const getBase = last => R.chain(
-  R.merge,
-  R.compose(
-    R.objOf('base'),
-    setBaseRamp(last),
-    R.ifElse(R.gt(30), R.always(30), R.identity),
-    R.reduceRight(R.subtract, 0),
-    R.props(['load', 'pvControl'])
-  )
-);
-
-const getBatteryBuffer = R.chain(
-  R.merge,
-  R.compose(
-    R.objOf('buffer'),
-    R.reduceRight(R.subtract, 0),
-    R.props(['pv', 'pvControl'])
-  )
-);
-
-const getBatteryStorage = R.chain(
-  R.merge,
-  R.compose(
-    R.objOf('storage'),
-    ([p, b, l]) => (p + b) - l,
-    R.props(['pvControl', 'base', 'load'])
-  )
-);
-
-const f = (acc, val, i) => {
-  return R.append(
-    R.merge(R.compose(
-      getBatteryStorage,
-      getBase(R.last(acc)),
-      getBatteryBuffer,
-      getPVcontrol(R.last(acc)),
-      getPV(i),
-      getLoad(i)
-    )({}),
-    val),
-    acc
+  const computePVRamp = last => R.ifElse(
+    checkRamp(PV_RAMP, last.pvControl),
+    R.identity,
+    R.clamp(R.subtract(last.pvControl, PV_RAMP), R.add(last.pvControl, PV_RAMP))
   );
-};
 
-let res = R.addIndex(R.reduce)(f, [], dates);
-console.log(res);
+  const computeBaseRamp = last => R.ifElse(
+    checkRamp(BASE_RAMP, last.base),
+    R.identity,
+    R.clamp(R.subtract(last.base, BASE_RAMP), R.add(last.base, BASE_RAMP))
+  );
 
-assert.deepEqual(res, expected);
+  const setBaseRamp = last => R.ifElse(
+    R.always(R.isNil(R.path(['base'], last))),
+    R.identity,
+    (x) => computeBaseRamp(last)(x)
+  );
+
+  const getPVcontrol = (last) => R.chain(
+    R.merge,
+    R.compose(
+      R.objOf('pvControl'),
+      R.ifElse(
+        R.always(R.or(NO_BATT, R.isNil(R.path(['pvControl'], last)))),
+        R.identity,
+        (x) => computePVRamp(last)(x)
+      ),
+      R.prop('pv')
+    )
+  );
+
+  const getBase = last => R.chain(
+    R.merge,
+    R.compose(
+      R.objOf('base'),
+      setBaseRamp(last),
+      R.ifElse(R.gt(BASE), R.always(BASE), R.identity),
+      R.reduceRight(R.subtract, 0),
+      R.props(['load', 'pvControl'])
+    )
+  );
+
+  const getBatteryBuffer = R.chain(
+    R.merge,
+    R.compose(
+      R.objOf('buffer'),
+      R.reduceRight(R.subtract, 0),
+      R.props(['pv', 'pvControl'])
+    )
+  );
+
+  const getBatteryStorage = R.chain(
+    R.merge,
+    R.compose(
+      R.objOf('storage'),
+      R.ifElse(
+        R.always(NO_BATT),
+        R.always(0),
+        ([p, b, l]) => (p + b) - l
+      ),
+      R.props(['pvControl', 'base', 'load'])
+    )
+  );
+
+  const f = (acc, val, i) => {
+    return R.append(
+      R.merge(R.compose(
+        getBatteryStorage,
+        getBase(R.last(acc)),
+        getBatteryBuffer,
+        getPVcontrol(R.last(acc)),
+        getPV(i),
+        getLoad(i)
+      )({}),
+      val),
+      acc
+    );
+  };
+
+  return R.addIndex(R.reduce)(f, [], DATES);
+}
+
+const res = computeOutput(data);
+const resNoBat = computeOutput(dataNoBat);
+
+assert.deepEqual(res, expected, '***BATTERY***');
+assert.deepEqual(resNoBat, expectedNoBat, '***NO BATTERY***');
